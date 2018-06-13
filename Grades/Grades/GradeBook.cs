06d0dfac9b79a8f14c126c9c4fb5236831994c8a
Laura Salcido <idlaurasalcido@gmail.com>
2018-06-13 16:35:13 -0700

    adding content, setting up classes

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        // class or cookie cutter used to create object
+        // constructor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            //store grade need access to it outside 
+            grades.Add(grade);
+        }
+
+
+        // classes that allow you to store multiple things
+        // what is it storing?
+        List<float> grades;
+    }
+}
